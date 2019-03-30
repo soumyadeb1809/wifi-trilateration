1562b34578f32ea49f138074aab5b95c6153cfbe
Soumya Deb <soumyadeb1809@gmail.com>
2019-03-31 03:27:38 +0530

    Updated success resposne of updateNodeLocation

--- updateNodeLocation.php
+++ updateNodeLocation.php
@@ -12,6 +12,7 @@ $lng = $_GET["lng"];
 $change_query = "UPDATE `node_location` SET latitude = $lat, longitude = $lng WHERE pk_location_id = (SELECT fk_location_id FROM wifidata WHERE ssid = '$ssid')";
 if(mysqli_query($conn, $change_query)){
     $response["result"] = "SUCCESS";
+    $response["message"] = "Position updated";
 }
 else{
     $response["result"] = "FAILED";
